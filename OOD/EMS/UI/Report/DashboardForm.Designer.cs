ed26b29a9849d702d76823d802ee7e3a0d1b988a
hamed.moghimi <shmoghimi70@gmail.com>
2014-07-12 13:57:47 +0430

    Forms renamed

--- OOD/EMS/UI/Report/DashboardForm.Designer.cs
+++ OOD/EMS/UI/Report/DashboardForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace OOD.ReportForms
 {
-    partial class TrainingResourceReport
+    partial class ScheduleReportForm
     {
         /// <summary>
         /// Required designer variable.
@@ -35,7 +35,13 @@
             this.label2 = new System.Windows.Forms.Label();
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.progress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.desc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -44,6 +50,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.dataGridView1);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.maskedTextBox2);
             this.groupBox1.Controls.Add(this.label2);
@@ -52,8 +59,8 @@
             this.groupBox1.Location = new System.Drawing.Point(12, 35);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.groupBox1.Size = new System.Drawing.Size(667, 280);
-            this.groupBox1.TabIndex = 7;
+            this.groupBox1.Size = new System.Drawing.Size(665, 281);
+            this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "گزارش‌گیری از برنامه اقدام";
             // 
@@ -85,6 +92,7 @@
             this.maskedTextBox2.TabIndex = 4;
             this.maskedTextBox2.Text = "۱۳۹۳۰۲۲۶";
             this.maskedTextBox2.ValidatingType = typeof(System.DateTime);
+            this.maskedTextBox2.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox2_MaskInputRejected);
             // 
             // label2
             // 
@@ -95,6 +103,7 @@
             this.label2.Size = new System.Drawing.Size(60, 26);
             this.label2.TabIndex = 3;
             this.label2.Text = "تا تاریخ :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // maskedTextBox1
             // 
@@ -105,6 +114,7 @@
             this.maskedTextBox1.TabIndex = 2;
             this.maskedTextBox1.Text = "۱۳۹۳۰۲۲۶";
             this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
             // 
             // label1
             // 
@@ -115,16 +125,64 @@
             this.label1.Size = new System.Drawing.Size(60, 26);
             this.label1.TabIndex = 1;
             this.label1.Text = "از تاریخ :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // TrainingResourceReport
+            // progress
+            // 
+            this.progress.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.progress.HeaderText = "درصد پیشرفت";
+            this.progress.Name = "progress";
+            this.progress.ReadOnly = true;
+            this.progress.Width = 107;
+            // 
+            // desc
+            // 
+            this.desc.HeaderText = "پایان برنامه";
+            this.desc.Name = "desc";
+            this.desc.ReadOnly = true;
+            // 
+            // amount
+            // 
+            this.amount.HeaderText = "شروع برنامه";
+            this.amount.Name = "amount";
+            this.amount.ReadOnly = true;
+            // 
+            // title
+            // 
+            this.title.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.title.HeaderText = "هدف";
+            this.title.Name = "title";
+            this.title.ReadOnly = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.title,
+            this.amount,
+            this.desc,
+            this.progress});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(653, 172);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // ScheduleReportForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
-            this.ClientSize = new System.Drawing.Size(691, 327);
+            this.ClientSize = new System.Drawing.Size(689, 328);
             this.Controls.Add(this.groupBox1);
-            this.Name = "TrainingResourceReport";
+            this.Name = "ScheduleReportForm";
+            this.Load += new System.EventHandler(this.ScheduleReportForm_Load);
             this.Controls.SetChildIndex(this.groupBox1, 0);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,12 +191,16 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.MaskedTextBox maskedTextBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
         private System.Windows.Forms.Label label1;
-
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn amount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn desc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn progress;
     }
 }
---
+++ OOD/EMS/UI/Report/MetricReportForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OOD.ReportForms
+{
+    public partial class ScheduleReportForm : OOD.Template
+    {
+        public ScheduleReportForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void ScheduleReportForm_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            foreach (object[] row in StaticData.schedules)
+                dataGridView1.Rows.Add(row);
+        }
+    }
+}
